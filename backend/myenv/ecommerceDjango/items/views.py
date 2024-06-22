cb018c232920bbcf8fd7319ffde7837f9348bc43
bsule <bilal.suleiman@gmail.com>
2024-06-22 00:49:45 -0700

    change to only allow admins to add, update, delete items

--- backend/myenv/ecommerceDjango/items/views.py
+++ backend/myenv/ecommerceDjango/items/views.py
@@ -1,18 +1,21 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .permissions import IsAdminOrReadOnly
 from .models import items
 from .serializers import ItemsSerializer
 
 
+# view for all items. Only admins can add items.
 class ItemListCreateView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     
     queryset = items.objects.all()
     serializer_class = ItemsSerializer
     
-
+    
+# view for a specific item. Only admins can update or delete items.
 class ItemDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     
     queryset = items.objects.all()
     serializer_class = ItemsSerializer
+    

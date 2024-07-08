28acdf449d3c392ccd23d4e2ec396878c0040eeb
bsule <bilal.suleiman@gmail.com>
2024-07-08 11:32:39 -0700

    working on items display

--- frontend/ecommerceReact/src/components/GetItems.jsx
+++ frontend/ecommerceReact/src/components/GetItems.jsx
@@ -17,7 +17,7 @@ function GetItems() {
     // const filteredItems = data.filter(item => item.category === 1);
 
     const pictureMap = data.map(item => (
-        <div key={item.id} className="flex mt-10 mr-5">
+        <div key={item.id} className="flex mt-10 mr-1 ml-1">
             <Link to={`/items/${item.id}`} className="relative bg-cover bg-center rounded-lg overflow-hidden w-[250px]">
                 <div className="absolute inset-0 z-0"></div>
                 <div className="relative z-10 flex flex-col text-black w-full text-pretty">
@@ -35,7 +35,7 @@ function GetItems() {
 
 
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row flex-wrap justify-content-center">
             {pictureMap}
         </div>
     );

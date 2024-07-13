fa64469aae4649fb8a28d4e34608dde9af2dee7a
bsule <bilal.suleiman@gmail.com>
2024-07-13 11:04:26 -0700

    working on cart

--- backend/myenv/ecommerceDjango/cart/models.py
+++ backend/myenv/ecommerceDjango/cart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from myapp.models import Product  # Assuming you have a Product model
+from items.models import items
 
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,11 +14,11 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    item = models.ForeignKey(items, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return f"{self.quantity} of {self.product.name}"
+        return f"{self.quantity} of {self.item.name}"
 
     def total_price(self):
-        return self.product.price * self.quantity
+        return self.item.price * self.quantity

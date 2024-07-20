f965d18865694a1406f4f66ac0814f977c35bba3
bsule <bilal.suleiman@gmail.com>
2024-07-20 08:56:40 +0200

    fixing cart slice

---
+++ frontend/ecommerceReact/src/redux/cartApis/cartApiSlice.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getAccessToken } from '../../utils/tokenManager';
+
+// Base URL for your API
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://127.0.0.1:8000/api/',
+    prepareHeaders: (headers) => {
+        const token = getAccessToken();
+        if (token) {
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+        return headers;
+    },
+});
+
+export const cartApiSlice = createApi({
+    reducerPath: 'cartApi',
+    baseQuery,
+    endpoints: (builder) => ({
+        addToCart: builder.mutation({
+            query: (item) => ({
+                url: 'cart/add/',
+                method: 'POST',
+                body: item,
+            }),
+        }),
+        viewCart: builder.query({
+            query: () => 'cart/',
+        }),
+        removeFromCart: builder.mutation({
+            query: (item) => ({
+                url: `cart/remove/`,
+                method: 'DELETE',
+                body: item,
+            }),
+        }),
+    }),
+});
+
+export const { useAddToCartMutation, useViewCartQuery, useRemoveFromCartMutation } = cartApiSlice;

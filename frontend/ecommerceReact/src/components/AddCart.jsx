3013989094da42b0bb2899e3c8f5d4a902085536
bsule <bilal.suleiman@gmail.com>
2024-07-30 00:50:42 +0300

    change to add to cart button

--- frontend/ecommerceReact/src/components/AddCart.jsx
+++ frontend/ecommerceReact/src/components/AddCart.jsx
@@ -1,17 +1,37 @@
 import { Button } from "react-bootstrap";
 import { useAddToCartMutation } from "../redux/cartApis/cartApiSlice";
+import { getAccessToken } from "../utils/tokenManager";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AddCart({ itemID }) {
     const [addToCart, {data, isLoading, error}] = useAddToCartMutation();
+    const navigate = useNavigate();
+    const accessToken = getAccessToken();
+    const stateAccess = useSelector((state) => state.token.accessToken);
+
+    // useEffect(() => {
+    //     console.log(accessToken);
+    //     if (!accessToken) {
+    //         navigate('/login');
+    //     }
+    // }, [stateAccess]);
 
     const handleAddToCart = async (event) => {
         event.preventDefault();
-        try {
-            await addToCart({ item: itemID, quantity: 1 }).unwrap();
-            console.log('Item added to cart');
+        if (!accessToken) {
+            navigate('/login');
         }
-        catch (error) {
-            console.error('Failed to add item to cart:', error);
+        else {
+
+            try {
+                await addToCart({ item: itemID, quantity: 1 }).unwrap();
+                console.log('Item added to cart');
+            }
+            catch (error) {
+                console.error('Failed to add item to cart:', error);
+            }
         }
     };
 

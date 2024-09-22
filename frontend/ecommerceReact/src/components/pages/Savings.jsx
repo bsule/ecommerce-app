b2b2b72950d6c7cad296220eac70c1f8503dea10
bsule <bilal.suleiman@gmail.com>
2024-09-22 12:03:15 -0700

    working on savings page

--- frontend/ecommerceReact/src/components/pages/Savings.jsx
+++ frontend/ecommerceReact/src/components/pages/Savings.jsx
@@ -1,5 +1,17 @@
-function Savings(){
+import { useGetItemsQuery } from "../../redux/authApis/getItemsApiSlice";
 
+function Savings(){
+    const {data, isLoading, error} = useGetItemsQuery();
+
+    if (isLoading) {
+        return (
+            <div>
+                <LoadingBarComponent isLoading={isLoading}/>
+            </div>
+        );
+    }
+
+    
 }
 
 export default Savings;
